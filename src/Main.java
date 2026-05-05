e1c329bd02d80f9a9d2e6cd86b6602124949e3fc
Dmitry-mysterious <dimagust5@gmail.com>
2026-05-05 15:40:48 +0300

    заменил else if на else во второй задаче

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ void main() {
         System.out.println("Установите облегченную версию приложения для Android по ссылке");
     } else if (clientOS == 0 && clientDeviceYear >= 2015) {
         System.out.println("Установите версию приложения для iOS по ссылке");
-    } else if (clientOS == 0 && clientDeviceYear < 2015) {
+    } else {
         System.out.println("Установите облегченную версию приложения для iOS по ссылке");
     }
 
